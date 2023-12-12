7fca0849e680f82cee3cbd3d18831fe9c1b0e713
Hairy Blue <pecision.nickimarty@gmail.com>
2023-12-12 08:14:21 +0800

    minor refactor in loginpage

--- src/Pages/Login/LoginPage.jsx
+++ src/Pages/Login/LoginPage.jsx
@@ -3,52 +3,53 @@ import { useState } from "react";
 import "./login.css";
 import Logo_CJC from "../../assets/Logo.png";
 
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "react-router-dom";
 export default function LoginPage() {
   const [emailUser, setEmailUser] = useState("");
   const [passwordUser, setPasswordUser] = useState("");
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = () => {
     const blog = {
       emailUser,
       passwordUser,
     };
-    console.log(blog); // Log the details to the console
+    console.log(blog);
+    navigate("/LandingPage");
   };
+
   const error_message = "Please login";
   return (
     <>
-      <form onChange={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div className="login_bg">
-          <div id="bg_blug">
-            <div id="login_box_main">
-              <div>
-                <img id="cjc_logo" src={Logo_CJC} alt="" />
-              </div>
-              <div id="error_spacer">
-                <p id="error_box">{error_message}</p>
-              </div>
-              <input
-                type="text"
-                value={emailUser}
-                onChange={(e) => setEmailUser(e.target.value)}
-                placeholder="email"
-              />
-              <input
-                id="bottom_input"
-                type="password"
-                placeholder="password"
-                onChange={(e) => setPasswordUser(e.target.value)}
-              />
-              <Link to={"/LandingPage"}>
-                <button id="login_button" type="button">
-                  Log in
-                </button>
-              </Link>
-              <p id="forgot_password-prompt">Forgot password?</p>
+          <div id="bg_blug"> </div>
+          <div id="login_box_main">
+            <div>
+              <img id="cjc_logo" src={Logo_CJC} alt="" />
             </div>
+            <div id="error_spacer">
+              <p id="error_box">{error_message}</p>
+            </div>
+            <input
+              type="email"
+              value={emailUser}
+              onChange={(e) => setEmailUser(e.target.value)}
+              placeholder="email"
+              required
+            />
+            <input
+              id="bottom_input"
+              type="password"
+              placeholder="password"
+              onChange={(e) => setPasswordUser(e.target.value)}
+              required
+            />
+            <button id="login_button" type="submit">
+              Log in
+            </button>
+            <p id="forgot_password-prompt">Forgot password?</p>
           </div>
         </div>
       </form>
